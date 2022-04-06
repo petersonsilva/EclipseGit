0792695f50d39a1597ce8513878f59c464e9a45d
Peterson <peterson.silva@gmail.com>
2022-04-06 15:13:14 -0300

    Primeiro Upload

---
+++ https:/github.com/petersonsilva/EclipseGit.git/projeto1/src/projeto1/HelloMundo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package projeto1;
+
+/**
+ * @author peterson
+ *
+ */
+public class HelloMundo {
+
+
+	public static void main(String[] args) {
+        System.out.println("Alo mundo!");
+
+	}
+
+}
